4f712d9e0744737cc808ba5b28b627c7a7a2402e
Josephpaz <josephpazjj@gmail.com>
2021-05-04 18:05:57 -0400

    Toggle component dark/light mode, switch

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,14 +1,18 @@
 import React, { useMemo } from "react";
 import { Container, Profile, Welcome, UserName } from "./styles";
 import emojis from "../../utils/emojis";
+import Toggle from "../Toggle";
+
 const MainHeader: React.FC = () => {
+  
   const emoji = useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length); //numero de emojis
     return emojis[indice];
   }, []);
+
   return (
     <Container>
-      <h1>Toogle</h1>
+      <Toggle />
       <Profile>
         <Welcome>Olá, {emoji}</Welcome>
         <UserName>Joseph Paz</UserName>
