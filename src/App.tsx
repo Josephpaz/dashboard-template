8902163a98d3db873505096bbc83c9091cfa33ba
Josephpaz <josephpazjj@gmail.com>
2021-05-04 19:18:47 -0400

    Menu component dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Layout from "./components/Layout";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import dark from "./styles/themes/dark";
-import light from "./styles/themes/light";
+// import light from "./styles/themes/light";
 
 const App: React.FC = () => {
   return (
