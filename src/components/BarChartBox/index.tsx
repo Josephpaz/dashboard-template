68abff8156e0b9591ba6b0801279e9eedd65f80c
Josephpaz <josephpazjj@gmail.com>
2021-06-01 21:04:14 -0400

    Handle State Rechart Graphics

--- src/components/BarChartBox/index.tsx
+++ src/components/BarChartBox/index.tsx
@@ -1,8 +1,59 @@
-import React from 'react';
+import React from "react";
+import { ResponsiveContainer, BarChart, Bar, Cell, Tooltip } from "recharts";
+import formatCurrency from "../../utils/formatCurrency";
+import {
+  Container,
+  SideLeft,
+  SideRight,
+  Legend,
+  LegendContainer,
+} from "./styles";
 
-const BarChartBox: React.FC = () => {
-    return (<div></div>);
+interface IBarChartBoxProps {
+  title: string;
+  data: {
+    name: string;
+    amount: number;
+    percent: number;
+    color: string;
+  }[];
 }
 
+const BarChartBox: React.FC<IBarChartBoxProps> = ({ title, data }) => {
+  return (
+    <Container>
+      <SideLeft>
+        <h2>{title}</h2>
+        <LegendContainer>
+          {data.map((indicator) => (
+            <Legend key={indicator.name} color={indicator.color}>
+              <div>{indicator.percent}%</div>
+              <span>{indicator.name}</span>
+            </Legend>
+          ))}
+        </LegendContainer>
+      </SideLeft>
+      <SideRight>
+        <ResponsiveContainer>
+          <BarChart data={data}>
+            <Tooltip
+              formatter={(value: number) => formatCurrency(Number(value))}
+              cursor={{ fill: "none" }}
+            />
+            <Bar dataKey="amount">
+              {data.map((indicator) => (
+                <Cell
+                  key={indicator.name}
+                  cursor="pointer"
+                  fill={indicator.color}
+                />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </SideRight>
+    </Container>
+  );
+};
 
 export default BarChartBox;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,7 +5,7 @@ import WalletBox from "../../components/WalletsBox";
 import MessageBox from "../../components/MessageBox";
 import PieChartBox from "../../components/PieChartBox";
 import HistoryBox from "../../components/HistoryBox";
-
+import BarChartBox from "../../components/BarChartBox";
 import { Container, Content } from "./styles";
 
 import expenses from "../../repositories/expenses";
@@ -102,6 +102,14 @@ const Dashboard: React.FC = () => {
           "Verifique seus gastos e tente cortar algumas coisas desnecessárias.",
         icon: sadImg,
       };
+    } else if (totalGains === 0 && totalExpenses === 0) {
+      return {
+        title: "Ops!",
+        description: "Neste mês, não há registros de entradas ou saídas.",
+        footerText:
+          "Parece que você não fez nenhum registro no mês e ano selecionado.",
+        icon: grinningImg,
+      };
     } else if (totalBalance === 0) {
       return {
         title: "Ufaa!",
@@ -117,23 +125,24 @@ const Dashboard: React.FC = () => {
         icon: happyImg,
       };
     }
-  }, [totalBalance]);
+  }, [totalBalance, totalGains, totalExpenses]);
 
   const relationExpensesVersusGains = useMemo(() => {
     const total = totalGains + totalExpenses;
-    const percentGains = (totalGains / total) * 100;
-    const percentExpenses = (totalExpenses / total) * 100;
+    const percentGains = Number(((totalGains / total) * 100).toFixed(1));
+    const percentExpenses = Number(((totalExpenses / total) * 100).toFixed(1));
+
     const data = [
       {
         name: "Entradas",
         value: totalGains,
-        percent: Number(percentGains.toFixed(1)),
+        percent: percentGains ? percentGains : 0,
         color: "#F7931B",
       },
       {
         name: "Saídas",
         value: totalExpenses,
-        percent: Number(percentExpenses.toFixed(1)),
+        percent: percentExpenses ? percentExpenses : 0,
         color: "#E44C4E",
       },
     ];
@@ -164,17 +173,67 @@ const Dashboard: React.FC = () => {
       });
 
     const total = amountRecurrent + amountEventual;
+    const percentRecurrent = Number(
+      ((amountRecurrent / total) * 100).toFixed(1)
+    );
+    const percentEventual = Number(((amountEventual / total) * 100).toFixed(1));
+
     return [
       {
         name: "Recorrentes",
         amount: amountRecurrent,
-        percent: Number(((amountRecurrent / total) * 100).toFixed(1)),
+        percent: percentRecurrent ? percentRecurrent : 0,
         color: "#F7931B",
       },
       {
         name: "Eventuais",
+        amount: amountEventual,
+        percent: percentEventual ? percentEventual : 0,
+        color: "#E44C4E",
+      },
+    ];
+  }, [monthSelected, yearSelected]);
+
+  const relationGainsRecurrentVersusEventual = useMemo(() => {
+    let amountRecurrent = 0;
+    let amountEventual = 0;
+
+    gains
+      .filter((expense) => {
+        const date = new Date(expense.date);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+
+        return month === monthSelected && year === yearSelected;
+      })
+      .forEach((gain) => {
+        if (gain.frequency === "recorrente") {
+          return (amountRecurrent += Number(gain.amount));
+        }
+
+        if (gain.frequency === "eventual") {
+          return (amountEventual += Number(gain.amount));
+        }
+      });
+
+    const total = amountRecurrent + amountEventual;
+
+    const percentRecurrent = Number(
+      ((amountRecurrent / total) * 100).toFixed(1)
+    );
+    const percentEventual = Number(((amountEventual / total) * 100).toFixed(1));
+
+    return [
+      {
+        name: "Recorrentes",
         amount: amountRecurrent,
-        percent: Number(((amountEventual / total) * 100).toFixed(1)),
+        percent: percentRecurrent ? percentRecurrent : 0,
+        color: "#F7931B",
+      },
+      {
+        name: "Eventuais",
+        amount: amountEventual,
+        percent: percentEventual ? percentEventual : 0,
         color: "#E44C4E",
       },
     ];
@@ -282,6 +341,15 @@ const Dashboard: React.FC = () => {
           data={HistoryData}
           lineColorAmountEntry="#F7931B"
           lineColorAmountOutput="#E44C4E"
+        />
+
+        <BarChartBox
+          title={"Saídas"}
+          data={relationExpensesRecurrentVersusEventual}
+        />
+        <BarChartBox
+          title={"Entradas"}
+          data={relationGainsRecurrentVersusEventual}
         />
       </Content>
     </Container>
